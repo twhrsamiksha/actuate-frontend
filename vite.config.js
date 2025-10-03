628ac85a9b75eb3b00a82259ebb4304166aab82a
twhrsamiksha <twhrsamiksha@gmail.com>
2025-10-03 15:09:05 +0530

    cors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/wp-json': {
+        target: 'http://3.6.14.227', // Your WordPress backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
